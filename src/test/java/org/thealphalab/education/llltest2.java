c34ce65ab3786100302de7545871501c89dac84f
lilulu <24271322632@qq.com>
2020-02-29 00:05:47 +0800

    lll update22

--- src/test/java/org/thealphalab/education/llltest2.java
+++ src/test/java/org/thealphalab/education/llltest2.java
@@ -5,5 +5,6 @@ import net.minidev.json.JSONUtil;
 public class llltest2 {
     public static void main(String[] args) {
         System.out.println("llll111");
+        System.out.println("222");
     }
 }
