2569c3d37c7873811868a96bb4f3e4edee7e2fc1
lilulu <24271322632@qq.com>
2020-02-29 18:27:12 +0800

    lll test

---
+++ src/test/java/org/thealphalab/education/Test.java
@@ -0,0 +1,17 @@
+package org.thealphalab.education;
+
+
+public class Test {
+
+  private long id;
+
+
+  public long getId() {
+    return id;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+}
